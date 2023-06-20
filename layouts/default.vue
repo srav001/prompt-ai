393a7a6a0bedb1ea6805472229cbe3f2e6ed6128
srav001 <sravansuresh1x@gmail.com>
2023-06-21 01:05:38 +0530

    feat: base design

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+	<main class="h-screen w-screen dark:text-soft-white dark:bg-neutral-900 bg-soft-white text-dark-gray py-10 px-20">
+		<header class="w-full flex justify-between items-center">
+			<h1 class="text-2xl font-bold">LOGO HERE</h1>
+			<button class="text-soft-white px-3 py-1 bg-dark-gray rounded-md">LOGIN</button>
+		</header>
+		<slot />
+	</main>
+</template>
+
+<style>
+* {
+	font-family: sans-serif;
+}
+</style>
